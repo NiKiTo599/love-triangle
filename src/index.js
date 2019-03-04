01dc41a3c3be6e9fdd869c10e403c604a4c92ba7
NiKiTo599 <akulichnikita0@gmail.com>
2019-03-04 22:28:21 +0300

    add decision

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences = []) { 
+  var count = 0;  
+  for (var i = 0; i<preferences.length; i++) { 
+      var pos = preferences[i] - 1;
+      if (pos === i) {
+          continue;
+      }
+      var pos1 = preferences[pos] - 1;
+      if (pos === pos1){
+          continue;
+      }
+      var pos2 = preferences[pos1] - 1;
+      if (pos2 === pos1){
+          continue;
+      }
+      if (pos2 === i) {
+          count++;
+      }
+   }
+   return count / 3;
 };
